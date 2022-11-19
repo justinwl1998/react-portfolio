328d9cd3c47a6ab15fd0987441f6c827155548f8
Justin Liu <justinwl2097@gmail.com>
2022-11-19 12:09:02 -0600

    trying to figure out why react won't render

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <div>
+            for the love of god help me
+            <Header />
+        </div>
+    );
+}
+
+export default App;
