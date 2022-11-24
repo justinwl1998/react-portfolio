b0f526a689507facbf507787aa243e2277517a91
Justin Liu <justinwl2097@gmail.com>
2022-11-23 18:23:40 -0600

    set initial css framework for about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,18 +2,31 @@ import React from 'react';
 
 // Todo: Add image of self to this
 
+const styles = {
+  containerStyle: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  flexItemStyle: {
+    margin: '5%'
+  }
+}
+
 export default function About() {
     return (
       <div>
-        <h1>About Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+        <h2>About Me</h2>
+        <div style={styles.containerStyle} className="container">
+          <img style={styles.flexItemStyle} src="https://via.placeholder.com/300"></img>
+          <p style={styles.flexItemStyle}>
+            Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+            Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+            mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+            lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+            in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+          </p>
+        </div>
       </div>
     );
   }
